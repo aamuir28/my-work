13b4187782ec8a874c51b39e61c215206d39d849
Aaron Muirhead <aamuir28@yahoo.com>
2023-05-18 21:33:28 -0400

    Started Python course

---
+++ Python Course/1. Spyder python files/4.0_print_function.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Print function.
+"""
+
+print('One, \'two\', "three", four, \n\\five\\ once, \'I\' caught a \n\tfish \'//alive\\\\\'')
---
+++ Python Course/Exercises/hello-world.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 21:01:28 2023
+
+@author: aamui
+"""
+
+print('This is a string')
+
+World = 53
+print(World)
+
+print('how are \n\tyou today?')
