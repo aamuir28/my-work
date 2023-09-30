4623e577fbed955291ff8530810f10db9079e086
Aaron Muirhead <aamuir28@yahoo.com>
2023-09-30 17:14:04 -0400

    front end and back end connected!

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/ruggadwebsite/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/ruggadwebsite/ecommerce/dao/ProductRepository.java
@@ -3,7 +3,8 @@ package com.ruggadwebsite.ecommerce.dao;
 import com.ruggadwebsite.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "product", path = "product")
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
